be15965d3f141953c627bfb13b9e743694ecbfaf
kyleconciso <kyleconciso@gmail.com>
2024-08-14 15:20:16 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Midi } = require('@tonejs/midi');
+const fs = require('node:fs');
+const path = require('node:path');
+
+// Loop all midi files
+const midiFolder = 'midi';
+const jsonFolder = 'json';
+
+try {
+	if (!fs.existsSync(midiFolder)) {
+		fs.mkdirSync(midiFolder);
+	}
+} catch (err) {
+	console.error(err);
+}
+
+try {
+	if (!fs.existsSync(jsonFolder)) {
+		fs.mkdirSync(jsonFolder);
+	}
+} catch (err) {
+	console.error(err);
+}
+
+
+const files = fs.readdirSync(midiFolder).forEach(file => {
+	var fileDir = path.join(midiFolder, file);
+	var data = fs.readFileSync(fileDir);
+	var midi = new Midi(data);
+	var outputFileDir = path.join(jsonFolder, path.parse(file).name+".json")
+	fs.writeFileSync(outputFileDir, JSON.stringify(midi.toJSON()));
+	console.log(fileDir+" -> "+outputFileDir);
+});
+
